9bd4db7eabb3b6ae9d8a310c3944178ae6cb6360
Alistair <agarioch@aquis.eu>
2024-07-15 14:30:34 +0100

    chore: add todo tests

--- lodash/index.test.js
+++ lodash/index.test.js
@@ -4,27 +4,47 @@ import { describe, it, vi, expect } from "vitest";
 // FILEPATH: /home/agarioch/interviews/lodash/index.test.js
 
 describe("memoizeFunc", () => {
-  it("should execute the function correctly", () => {
+  it("the returned function should execute correctly", () => {
     const add = (a, b) => a + b;
     const memoizedAdd = memoizeFunc(add);
 
     expect(memoizedAdd(2, 3)).toBe(5);
-    expect(memoizedAdd(2, 3)).toBe(5); // Result should be memoized
     expect(memoizedAdd(4, 5)).toBe(9);
+
+    const countLetters = (str, letter) =>
+      str.split("").filter((char) => char === letter).length;
+    const memoizedCountLetters = memoizeFunc(countLetters);
+
+    expect(memoizedCountLetters("hello", "l")).toBe(2);
   });
 
-  it("should memoize the results", () => {
+  it("the returned function should cache results and return from cache when same arguments are passed", () => {
     const mockFn = vi.fn();
     const memoizedFn = memoizeFunc(mockFn);
 
+    // Basic test
     memoizedFn(1, 2);
     memoizedFn(1, 2);
     memoizedFn(1, 2);
 
     expect(mockFn).toHaveBeenCalledTimes(1); // Function should be called only once
+
+    // Test with falsy value
+    memoizedFn(2, -2);
+    memoizedFn(2, -2);
+
+    expect(mockFn).toHaveBeenCalledTimes(2);
   });
 
-  it.todo("should work with any primitive arguments", () => {});
+  it.todo(
+    "calling the memoized function with different arguments should call the original function",
+    () => {}
+  );
+
+  it.todo(
+    "the returned function works with any primitive type of arguments",
+    () => {}
+  );
 });
 
 describe("throttle", () => {
